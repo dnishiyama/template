bc7c3bc50e3b19ad259fb3722b1ef68dbcbf22f6
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2024-04-17 16:32:24 -0400

    update lint for escaped text

--- tooling/eslint/nextjs.js
+++ tooling/eslint/nextjs.js
@@ -3,6 +3,7 @@ const config = {
   extends: ["plugin:@next/next/core-web-vitals"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react/no-unescaped-entities": ["error", { forbid: [">", "}"] }],
     "@typescript-eslint/require-await": "off",
   },
 };
