f99c5300cf270e0759fafccd916eab4d36957bb3
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2024-05-06 10:59:32 -0400

    allow for multiple channels for migrations

--- packages/db/src/migrate.ts
+++ packages/db/src/migrate.ts
@@ -9,12 +9,19 @@ if (!databaseUrl) throw new Error("DATABASE_URL is not defined");
 
 // for migrations
 const sql = postgres(databaseUrl);
+const drizzleMigrationClient = drizzle(sql);
 
 const main = async () => {
-  const drizzleMigrationClient = drizzle(sql);
+  if (!databaseUrl) return;
+  if (process.env.CI) return;
+  if (!drizzleMigrationClient) return;
 
+  // get the channel from the database url (e.g. postgres://localhost:5432/acme_dev -> dev; postgres://localhost:5432/acme -> acme)
+  const channel = databaseUrl.split("/").slice(-1)[0]?.split("_").slice(-1)[0];
+
+  console.log("Migrating channel", channel);
   return await migrate(drizzleMigrationClient, {
-    migrationsTable: "migrations",
+    migrationsTable: `__drizzle_migrations_${channel}`,
     migrationsFolder: "drizzle",
   });
 };
