741869406a560f1d5b5685bda36645ff8559b3ba
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2024-05-11 20:45:51 -0400

    eas env

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -9,17 +9,19 @@ const extra: EasExtra = {
     // TODO PUBLISH: Change this to your own EAS project ID
     projectId: "9ae04bbe-5ef6-4ea9-b148-ac984152b1b4",
   },
-  apiUrl: "http://create-t3-turbo-jade.vercel.app",
+  apiUrl:
+    // https://docs.expo.dev/build-reference/variables/#setting-plaintext-environment-variables-in-easjson
+    process.env.EXPO_PUBLIC_API_URL ?? "http://create-t3-turbo-jade.vercel.app",
 };
 
 const defineConfig = (): ExpoConfig => ({
-  name: "t3template",
+  name: process.env.APP_NAME ?? "t3template",
   slug: "t3template",
   scheme: "t3template",
   version: "0.1.0",
   owner: "mountain_dev",
   orientation: "portrait",
-  icon: "./assets/icon.png",
+  icon: process.env.APP_ICON ?? "./assets/icon.png",
   userInterfaceStyle: "automatic",
   splash: {
     image: "./assets/icon.png",
