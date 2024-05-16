a3861af3d5f2c8ef2298b237b20e0fdee67f4a67
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2024-05-16 09:40:15 -0400

    update prettier

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -6,6 +6,13 @@ import { fileURLToPath } from "url";
 
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
+  // arrowParens: "always",
+  // printWidth: 80,
+  // singleQuote: true,
+  // jsxSingleQuote: true,
+  // semi: false,
+  // trailingComma: "all",
+  // tabWidth: 2,
   plugins: [
     "@ianvs/prettier-plugin-sort-imports",
     "prettier-plugin-tailwindcss",
